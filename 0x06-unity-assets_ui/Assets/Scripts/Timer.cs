19af1b8b65711df80610ebaab7854c74d34fde63
nataliamedinat <nataliamedinabotero@gmail.com>
2020-06-24 17:51:16 -0500

    duplicate

---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private float startTime;
+   // private bool start = false;
+
+    //public void StartTime()
+    //{
+    //    start = true;
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (start)
+        {
+        float t = Time.time - startTime; // Store the tima since the timer has started
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        TimerText.text = minutes + ":" + seconds;
+        }
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.gameObject.GetComponent<Timer>().enabled = true;
+        }
+    }
+}
+
+    
