48ec32164894c42ae172f8db7d4ab685c46c1f29
nataliamedinat <nataliamedinabotero@gmail.com>
2020-06-24 17:38:16 -0500

    Tetrominos, grid, and principal movements

---
+++ 2D-clone/Assets/Scripts/Tetramino.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetramino: MonoBehaviour
+{
+    private float previousTime; // For vertical movement
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+        }
+
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime)) // When down key is pressed the tetromino falls more fast
+        {
+            transform.position += new Vector3(0, -1, 0); //Made the tetromino fall
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+            previousTime = Time.time;
+        }
+    }
+    // Check if the tetromino is inside the grid
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height) // If it is bigger than the grid size, will return false
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
