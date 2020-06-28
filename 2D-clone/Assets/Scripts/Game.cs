84799693f2e076ff337125cfe4751d660a1744af
nataliamedinat <nataliamedinabotero@gmail.com>
2020-06-28 05:10:49 -0500

    Final details clearing the rows, script game over

--- 2D-clone/Assets/Scripts/Game.cs
+++ 2D-clone/Assets/Scripts/Game.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
-{
-    public static int gridWidth = 10;
-    public static int gridHeight = 20;
+{ 
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +16,5 @@ public class Game : MonoBehaviour
         
     }
 
-
-    public bool CheckIsInsideGrid (Vector2 pos)
-    {
-        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
-    }
-
-    public Vector2 Round (Vector2 pos)
-    {
-        return new Vector2 (Mathf.Round(pos.x), Mathf.Round(pos.y));
-    }
 
 }
--- 2D-clone/Assets/Scripts/Tetramino.cs
+++ 2D-clone/Assets/Scripts/Tetramino.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Tetramino: MonoBehaviour
+public class Tetramino : MonoBehaviour
 {
+
+    public Vector3 rotationPoint;
     private float previousTime; // For vertical movement
     public float fallTime = 0.8f;
     public static int height = 20;
     public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +42,10 @@ public class Tetramino: MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate(0, 0, 90);
+            if (!ValidMove())
+            {
+                transform.Rotate(0, 0, -90);
+            }
         }
 
 
@@ -47,10 +55,93 @@ public class Tetramino: MonoBehaviour
             if (!ValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetramino>().NewTetromino();
             }
             previousTime = Time.time;
         }
+
+        void CheckForLines()
+        {
+            for (int i = height -1; i >= 0; i--)
+            {
+                if(HasLines(i))
+                {
+                    DeleteLines(i);
+                    RowDown(i);
+
+                }
+            }
+        }
+
+        bool HasLines(int i)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                if (grid[j, i] == null)
+                    return false;
+            }
+            return true;
+        }
+
+        void DeleteLines(int i)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Destroy(grid[j, i].gameObject);
+                grid[j, i] = null;
+            }
+        }
+
+        void RowDown(int i)
+        {
+            for (int y = i; y < height; y++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (grid[j, y] != null)
+                    {
+                        grid[j, y - 1] = grid[j, y];
+                        grid[j, y] = null;
+                        grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                    }
+                }
+            }
+        }
+
+        void AddToGrid()
+        {
+            foreach (Transform children in transform)
+            {
+                int roundedX = Mathf.RoundToInt(children.transform.position.x);
+                int roundedY = Mathf.RoundToInt(children.transform.position.y);
+                grid[roundedX, roundedY] = children;
+            }
+            CheckEndGame();
+        }
     }
+
+    void CheckEndGame()
+    {
+        // For every block in the column
+        for (int j = 0; j < width; j++)
+        {
+            // Check to see if there are any blocks in the highest row
+            if (grid[j, height - 1] != null)
+            {
+                // If there are blocks at the top, the game is over
+                GameOver();
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     // Check if the tetromino is inside the grid
     bool ValidMove()
     {
@@ -62,6 +153,11 @@ public class Tetramino: MonoBehaviour
             if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height) // If it is bigger than the grid size, will return false
             {
                 return false;
+            }
+            if (grid[roundedX, roundedY] != null) // if its already taken
+            {
+                return false;
+       
             }
         }
         return true;
