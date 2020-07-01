2ba01a3de859f51dc59f1f3ebf1e0b60444282c2
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-01 04:43:57 -0500

    Option and mainMenu scripts

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void PreviousScene()
+    {
+        PlayerPrefs.SetString("Back", SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
+   
