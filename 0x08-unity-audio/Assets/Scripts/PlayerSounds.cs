029901d79c4c98e7f4943edb52095796d00197c0
Natalia <51713603+nataliamedinat@users.noreply.github.com>
2020-07-22 18:18:00 -0500

    sound to the background

---
+++ 0x08-unity-audio/Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip walkgrass, walkrock;
+    private AudioSource audiosrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        audiosrc = GetComponent<AudioSource>();
+    }
+
+    // start running on rock sound
+    public void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            audiosrc.PlayOneShot(walkrock);
+             Debug.Log("estoy en rock");
+        }
+    }
+}
